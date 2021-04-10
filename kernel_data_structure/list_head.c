be30724309d6534658e14c2065a9095281c8f450
SexyEunwoo <dmsdndla1@gmail.com>
2021-04-10 16:39:13 +0900

    feat: Add list_head study

--- kernel_data_structure/list_head.c
+++ kernel_data_structure/list_head.c
@@ -1,29 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "types.h"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/list.h>
 
-LIST_HEAD(pList);
-INIT_LIST_HEAD(&pList);
-
-typedef struct _SPerson {
-	int age;
-	char name[10];
-	struct list_head list;
-} Person;
-
-int main(void)
+static int __init list_init(void)
 {
-	Peson* ptrP;
-	Person p1 = { 20, "FFF" };
-	Person p2 = { 30, "ASD" };
-
-	list_add_tail(&p1.list, &pList);
-	list_add_tail(&p2.list, &pList);
-
-	list_for_each_entry(ptrP, &pList, list)
-	{
-		printf("struct Person[%p] age is %d, name is %s \n", ptrP, ptrP->age, ptrP->name);
-	}
-
+	pr_info("list_init \n");
 	return 0;
 }
+
+static void __exit list_exit(void)
+{
+	pr_info("list_exit \n");
+}
+
+
+module_init(list_init);
+module_exit(list_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EUNWOO");
+MODULE_DESCRIPTION("LIST_HEAD TEST");
